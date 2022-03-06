9c7001cda12a313d8c8f33038db831475f4941b6
Jason Chou <j345593111@gmail.com>
2022-03-05 20:56:22 -0800

    Update Q3.sql

--- Q3.sql
+++ Q3.sql
@@ -12,3 +12,19 @@ Create Table Products_price_history (
   CONSTRAINT FK_ProductName FOREIGN KEY (Name)
   REFERENCES Products(Name)
 );
+
+-- testing data for Products
+INSERT INTO Products VALUES ('AAA', 'testing');
+INSERT INTO Products VALUES ('BBB', 'testing');
+
+-- testing data for Products_price_history
+INSERT INTO Products_price_history VALUES ('AAA', 100, TO_TIMESTAMP('2022-02-09 07:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP('2022-02-10 07:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO Products_price_history VALUES ('AAA', 150, TO_TIMESTAMP('2022-02-10 07:00:00', 'YYYY-MM-DD HH24:MI:SS'), null);
+INSERT INTO Products_price_history VALUES ('BBB', 200, TO_TIMESTAMP('2022-03-02 07:00:00', 'YYYY-MM-DD HH24:MI:SS'), null);
+
+--query sql
+SELECT p.Name, p.Category, h.Price, h.Start_date, h.End_date
+FROM Products p
+INNER JOIN Products_price_history h
+ON p.Name=h.Name
+ORDER BY h.Start_date
